36ccbcc500b1d131d0996e669efbeedfadcc4897
Gustavo Valmaña Villalonga <gustavo.valmana@alegra.com>
2024-09-16 15:09:11 -0500

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import LoginService from "./services/LoginService";
+import LoginComponent from "./LoginComponent";
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const loginService: LoginService = new LoginService();
+const setToken = (token: string) => {
+  console.log(`received token: ${token}`);
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<LoginComponent loginService={loginService} setToken={setToken} />);
